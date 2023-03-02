acbc59680e4cf9d4d4c98ad42f65230eeb2bb877
Sezer Ökmen <szrokmn34@gmail.com>
2023-03-02 17:09:17 +0300

    doctors added

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,9 +1,22 @@
+import { Col, Container, Row } from "react-bootstrap";
 
-
-const Doctors = () => {
+const Doctors = ({ doctors }) => {
   return (
-    <div>Doctors</div>
-  )
-}
+    <Container className="p-2">
+      <h3 className="display-6 mb-3" style={{ color: "purple" }}>
+        Our Doctors
+      </h3>
+      <Row className="justift-content-center">
+        {doctors.map((dr) => (
+          <Col key={dr.id}>
+            <img src={dr.img} alt="dr.name" xs={6} sm={4} md={3}/>
+            <h4>{dr.name}</h4>
+            <h5>{dr.dep}</h5>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
 
-export default Doctors
+export default Doctors;
