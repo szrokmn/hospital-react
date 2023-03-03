7c5a6c00b3cf68039d45606d481a30c8887d3a98
Sezer Ökmen <szrokmn34@gmail.com>
2023-03-03 19:45:42 +0300

    handleSubmit func. added

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,23 +1,33 @@
 import React, { useState } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
 
-export const AddModal = ({ show, handleClose }) => {
+export const AddModal = ({ show, handleClose, drName }) => {
+
+  const [patientName, setPatientName] = useState("")
+  const [date, setDate] = useState("")
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  handleClose()
+}
+console.log(patientName, date)
+
   return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Appointment for...</Modal.Title>
+          <Modal.Title>Appointment for {drName}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="name">
               <Form.Label>Patient Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter your name" />
+              <Form.Control type="text" placeholder="Enter your name" onChange={(e)=> setPatientName(e.target.value)}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="datetime">
               <Form.Label>Day&Time</Form.Label>
-              <Form.Control type="datetime-local" />
+              <Form.Control type="datetime-local" onChange={(e)=> setDate(e.target.value)}/>
             </Form.Group>
             <div className="text-center ">
               <Button variant="success" type="submit" className="me-2">
--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -5,15 +5,17 @@ import { AddModal } from "./AddModal"
 const Doctors = ({ doctors }) => {
 
     const [show, setShow] = useState(false);
+    const [selectedDrName, setSelectedDrName] = useState("")
 
     // const handleClose = () => setShow(false);
     // const handleShow = () => setShow(true);
 
-    const handleClick = () => {
+    const handleClick = (drName) => {
         // handleShow()
         setShow(true)
+        setSelectedDrName(drName)
     }
-
+    console.log(selectedDrName)
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-3" style={{ color: "purple" }}>
@@ -22,14 +24,22 @@ const Doctors = ({ doctors }) => {
       <Row className="justift-content-center">
         {doctors.map((dr) => (
           <Col key={dr.id}>
-            <img src={dr.img} alt="dr.name" xs={6} sm={4} md={3} onClick={handleClick}/>
+            <img src={dr.img} alt="dr.name" className="img-thumbnail doctor-img" xs={6} sm={4} md={3} onClick={
+              () => handleClick(dr.name)
+              // ()=> {
+              //   setShow(true)
+              //   selectedDrName(dr.Name)
+              // }
+              }/>
             <h4>{dr.name}</h4>
             <h6>{dr.dep}</h6>
           </Col>
         ))}
       </Row>
       {/* <AddModal show={show} handleClose={handleClose}/> */}
-      <AddModal show={show} handleClose={()=> setShow(false)}/>
+      <AddModal show={show} handleClose={()=> setShow(false)} 
+      drName={selectedDrName}
+      />
     </Container>
   );
 };
